86dcfbdb8ec51eb1ff934e8761aabffc1ac2b6ec
Imagekafle10 <hpimage10@gmail.com>
2025-11-17 21:41:30 +0545

    Update: Profile Update Route

--- backend/routes/auth/auth.route.js
+++ backend/routes/auth/auth.route.js
@@ -5,12 +5,21 @@ const {
   validateLogin,
 } = require("../../middleware/authValidation");
 const auth = require("../../middleware/auth");
+const upload = require("../../utils/upload");
 const router = express.Router(); // Corrected
 
 // Example route
 router.post("/signup", validateSignup, authController.signup);
 
 router.post("/login", validateLogin, authController.login);
+router.put(
+  "/updateprofile",
+  upload.single("photo"),
+  auth,
+  authController.updateProfile
+);
+
+router.get("/me", auth, authController.getUserDetailsById);
 
 router.post("/logout", auth, authController.logout);
 
