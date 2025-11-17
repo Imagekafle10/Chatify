6969d24303ec5f87482a92bdd615ea6505d1a02a
Imagekafle10 <hpimage10@gmail.com>
2025-11-17 20:10:29 +0545

    Refactor: Login Validation

--- backend/middleware/authValidation.js
+++ backend/middleware/authValidation.js
@@ -1,4 +1,4 @@
-import Joi from "joi";
+const Joi = require("joi");
 
 const signupSchema = Joi.object({
   fullName: Joi.string()
@@ -24,7 +24,7 @@ const signupSchema = Joi.object({
 });
 
 // Middleware function
-export const validateSignup = (req, res, next) => {
+const validateSignup = (req, res, next) => {
   const { error } = signupSchema.validate(req.body, { abortEarly: false });
 
   if (error) {
@@ -33,3 +33,31 @@ export const validateSignup = (req, res, next) => {
 
   next();
 };
+
+const LoginSchema = Joi.object({
+  email: Joi.string().email().required().messages({
+    "string.email": "Email must be a valid email address",
+    "string.empty": "Email is required",
+  }),
+  password: Joi.string()
+    .pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/)
+    .required()
+    .messages({
+      "string.pattern.base":
+        "Password must be at least 8 characters, include uppercase, lowercase, number, and special character",
+      "string.empty": "Password is required",
+    }),
+});
+
+// Middleware function
+const validateLogin = (req, res, next) => {
+  const { error } = LoginSchema.validate(req.body, { abortEarly: false });
+
+  if (error) {
+    return next(new Error("Validation Failed"));
+  }
+
+  next();
+};
+
+module.exports = { validateSignup, validateLogin };
